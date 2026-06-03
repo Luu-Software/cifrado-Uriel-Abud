a1436c712a3f4fdb4d89e1ba994b247b20122af5
Uriel-Abud <uriabud@gmail.com>
2026-06-03 16:00:43 -0300

    Terminé la Consigna Base y traté de avanzar un poco con la Consigna Manija

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { preguntar } from './lib/consola.ts';
 import { cifrar, descifrar } from './lib/funcionesCriptográficas.ts';
 
-let palabra: string = ''; // Preguntar por la palabra a cifrar o descifrar
-let claveTexto: string = ''; // Preguntar por la clave de cifrado (número)
-let accion: string = ''; // Preguntar si se desea cifrar o descifrar (cifrar/descifrar)
+let palabra: string = preguntar('Palabra a cifrar o descifrar: '); // Preguntar por la palabra a cifrar o descifrar
+
+let claveTexto: string = preguntar('Clave de cifrado (número): '); // Preguntar por la clave de cifrado (número)
+let clave: number = Number(claveTexto);
+
+let accion: string = preguntar('¿Desea cifrar o descifrar? cifrar/descifrar: '); // Preguntar si se desea cifrar o descifrar (cifrar/descifrar)
 
 let resultado: string = ''; // COMPLETAR
+
+if (accion === "cifrar") {
+    resultado = cifrar(palabra, clave)
+}
+else if (accion === "descifrar") {
+    resultado = descifrar(palabra, clave)
+}
+
 console.log(resultado);
