79062099e8caf9f4586eaff20c36e685b0549835
Uriel-Abud <uriabud@gmail.com>
2026-06-23 14:30:05 -0300

    Terminé la Consigna manija

--- src/manija.ts
+++ src/manija.ts
@@ -5,10 +5,26 @@ let tarjetaNúmero: number = Number (tarjetaTexto);
 
 console.log (tarjetaNúmero);
 
-function obtenerPrefijo(tarjetaNúmero: number, tamañoPrefijo: number): number {
-  tarjetaNúmero
-  return 0; // COMPLETAR
+function obtenerPrefijo(número: number, longitudPrefijo: number): number {
+  return Math.floor (número / (10**(16 - longitudPrefijo)));; // COMPLETAR
 }
 
-let red: string = ''; // COMPLETAR
+let prefijo1: number = obtenerPrefijo (tarjetaNúmero, 1);
+let prefijo2: number = obtenerPrefijo (tarjetaNúmero, 2);
+
+let red: string = ""; // COMPLETAR
+
+if (prefijo2 === 34 || prefijo2 === 37) {
+  red = "AMEX";
+}
+else if (prefijo1 === 4) {
+  red = "VISA";
+}
+else if (prefijo2 === 51 || prefijo2 === 52 || prefijo2 === 53 || prefijo2 === 54 || prefijo2 === 55) {
+  red = "MASTERCARD";
+}
+else {
+  red = "Desconocida";
+}
+
 console.log(red);
